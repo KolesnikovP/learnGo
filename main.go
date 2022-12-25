a02dc4ad750bf41a36614a3f38ba10127180dc76
Petr <ptrklsnkv@gmail.com>
2022-12-25 07:57:01 +0300

    constants and iota

--- main.go
+++ main.go
@@ -13,7 +13,16 @@ func main() {
 		sunday
 	)
 
-	fmt.Println(monday, tuesday, wednesday, thursday, friday, saturday, sunday)
+	const (
+		A int = 45
+		B
+		C float32 = 3.3
+		D
+	)
+
+	fmt.Println(monday, tuesday, wednesday, thursday, friday, saturday, sunday) // 0 1 2 3 4 5 6
+
+	fmt.Println(A, B, C, D) // Выведет: 45 45 3.3 3.3
 }
 
 /*
